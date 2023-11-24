9ca916fbc726eac1305a4247709eef99af507274
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-11-24 12:08:07 +0100

    update

--- src/main/java/com/sdia/application/App6.java
+++ src/main/java/com/sdia/application/App6.java
@@ -1,22 +1,22 @@
 package com.sdia.application;
 
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
+
+import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
-public class App4 {
+public class App6 {
     public static void main(String[] args) throws StreamingQueryException, TimeoutException {
-        SparkSession ss=SparkSession.builder().appName("Sockets").master("local[*]").getOrCreate();
+        SparkSession ss=SparkSession.builder().appName("csv").master("local[*]").getOrCreate();
 
-        Dataset<Row> df =ss.readStream().format("socket").option("host","localhost")
-                .option("port",8080)
-                .load();
+        //Dataset<Row> df=ss.read().option("header",true).option("inferSchema",true).csv("products.csv");
 
-        StreamingQuery query=df.writeStream().outputMode("append").format("console").start();
-        query.awaitTermination();
 
     }
 }
